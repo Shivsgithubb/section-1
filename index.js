abf6d337e85f2fa205bf1be57976ad0e2e93f8c3
Shivsgithubb <vs6828208@gmail.com>
2024-07-21 20:33:47 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('hello javascript');
+console.log('nice');
+
+num = 72653;
+
+myname = 'shivendra'
+
+console.log(myname);
+
+console.log(typeof num);
+
+console.log(typeof myname);
+
+console.log(1=='1')
+console.log(1==='1')
+
